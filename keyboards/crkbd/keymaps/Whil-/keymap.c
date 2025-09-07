c869b08a271a56bf05c79ad15ffc5bd511c7e57c
Gustav Wikström <gustav@whil.se>
2025-09-07 23:59:31 +0200

    Switch LALT and LSFT to make it easier to end sentences
    Typing space and uppercase directly after each other in the end of
    each sentence must be done with more fingers than only left thumb.

--- keyboards/crkbd/keymaps/Whil-/keymap.c
+++ keyboards/crkbd/keymaps/Whil-/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   (
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT, KC_BSPC,
       KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_LGUI,
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                          KC_LALT,  KC_SPC, KC_LSFT,      LOWER,   RAISE,  KC_RALT
+      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+                                          KC_RALT,  KC_SPC, KC_LSFT,      LOWER,   RAISE,  KC_LSFT
   ),
 
   // Lower
