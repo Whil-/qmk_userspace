589a632ec1e7c51f1f741f44b30c4c39510b450a
Gustav Wikström <gustav.wikstrom@swedbank.se>
2025-09-08 16:04:52 +0200

    Move some more keys aruond after testing it out for a day
    - Move esc so it's a one-hand key
    - Switch places between 0 and . so it will be easier to type numbers
      including 0, without having to reposition the hand between typing 0
      and the rest of the numbers.
    - Switch space and shift on left thumb cluster. The thumb is always
      resting on the middle key and it wuold be good it that key is the
      one that does most of the work.

--- keyboards/crkbd/keymaps/Whil-/keymap.c
+++ keyboards/crkbd/keymaps/Whil-/keymap.c
@@ -30,22 +30,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3
   (
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT, KC_BSPC,
-      KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_LGUI,
-      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                          KC_RALT, KC_LSFT,  KC_SPC,      LOWER,   RAISE,  KC_LSFT
+      KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_RALT,
+      KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+                                          KC_LALT,  KC_SPC, KC_LSFT,      LOWER,   RAISE,  KC_LSFT
   ),
 
   // Lower
   [_LOWER] = LAYOUT_split_3x6_3(
       _______,  KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR,                      KC_PSLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, _______,
       _______, KC_MINS, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,                      KC_PMNS,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
-      _______, KC_BSLS, KC_SCLN, KC_LBRC, KC_RBRC,  KC_EQL,                        KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, KC_PENT,
+      _______, KC_BSLS, KC_SCLN, KC_LBRC, KC_RBRC,  KC_EQL,                      KC_PDOT,   KC_P1,   KC_P2,   KC_P3,   KC_P0, KC_PENT,
                                           _______, _______, _______,    _______, _______, _______
   ),
 
   // Raise with swedish codes
   [_RAISE] = LAYOUT_split_3x6_3(
-      _______, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ESC,                      KC_PSCR,  SWE_AA,  SWE_AE,  SWE_OE, XXXXXXX, _______,
+      _______, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PSCR,  SWE_AA,  SWE_AE,  SWE_OE,  KC_ESC, _______,
       _______, KC_UNDS, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                       KC_INS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______,
       _______, KC_PIPE, KC_COLN, KC_LCBR, KC_RCBR, KC_PLUS,                       KC_DEL, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______,
                                           _______, _______, _______,    _______, _______, _______
@@ -57,7 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_APP,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,
       KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,
                                           XXXXXXX, XXXXXXX, XXXXXXX,    _______, _______, XXXXXXX
-                                      //`--------------------------'  `--------------------------'
   )
 };
 
