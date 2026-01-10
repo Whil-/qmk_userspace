df0078aabda2c30e3bfd868150ffd397491e7289
Gustav Wikström <gustav.wikstrom@swedbank.se>
2026-01-10 09:52:32 +0100

    Another iteration

--- keyboards/crkbd/keymaps/whil46/keymap.c
+++ keyboards/crkbd/keymaps/whil46/keymap.c
@@ -37,34 +37,31 @@ enum layers {
 
 // Define keys to keep them short in the config below
 
-#define LOWER    LT(_LOWER, KC_SPC)
+#define LOWER    LT(_LOWER, KC_MINS)
 #define RAISE    LT(_RAISE, KC_SPC)
-#define GUI_ENT  LGUI_T(KC_ENT)
-#define ALT_SPC  LALT_T(KC_SPC)
-#define ALT_EQL  LALT_T(KC_EQL)
-#define SFT_ENT  LSFT_T(KC_ENT)
-#define ALT_QUO  LALT_T(KC_QUOT)
+#define GUI_SPC  LGUI_T(KC_SPC)
 #define GUI_GRV  LGUI_T(KC_GRV)
-#define GUI_BSL  LGUI_T(KC_BSLS)
-#define GUI_PIP  LGUI_T(KC_BSLS)
+#define ALT_QUO  LALT_T(KC_QUOT)
+#define SFT_EQL  LSFT_T(KC_EQL)
+#define ALT_ENT  LALT_T(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3_ex2(
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   KC_NO,      KC_NO,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
       KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  QK_REP,     QK_REP,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, ALT_QUO,
       GUI_GRV,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                          KC_LALT,   LOWER, KC_LSFT,    KC_LGUI,   RAISE,  SFT_ENT
+                                          ALT_ENT, GUI_SPC, KC_LSFT,      LOWER,   RAISE,  SFT_EQL
   ),
   [_LOWER] = LAYOUT_split_3x6_3_ex2(
-      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______,    _______, KC_PPLS,   KC_P7,   KC_P8,   KC_P9, KC_COLN, _______,
-      _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,    _______, KC_PDOT,   KC_P4,   KC_P5,   KC_P6,   KC_P0, ALT_EQL,
-      _______, KC_UNDS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                      KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, _______,
+      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______,    _______,  KC_EQL,   KC_P7,   KC_P8,   KC_P9, KC_SCLN, _______,
+      _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,    _______, KC_PDOT,   KC_P4,   KC_P5,   KC_P6,   KC_P0, ALT_QUO,
+      _______, KC_UNDS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                      KC_MINS,   KC_P1,   KC_P2,   KC_P3, KC_SLSH, _______,
                                           _______, _______, _______,    _______, _______, _______
   ),
   [_RAISE] = LAYOUT_split_3x6_3_ex2(
       _______, KC_PSCR,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,    _______,  KC_INS,  SWE_AA,  SWE_AE,  SWE_OE, XXXXXXX, _______,
       _______, KC_BSLS,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,    _______,  KC_DEL, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_LALT,
-      _______, KC_PIPE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,                       KC_ESC, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______,
+      KC_LGUI, KC_PIPE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,                       KC_ESC, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______,
                                           _______, _______, _______,    _______, _______, _______
   ),
   [_ADJUST] = LAYOUT_split_3x6_3_ex2(
